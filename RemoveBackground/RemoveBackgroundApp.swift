38fac0a7826544e6be81f7af71111dc9d8e8d753
AnTran <cao.an99@gmail.com>
2024-05-03 09:59:47 +0700

    Initial Commit

---
+++ RemoveBackground/RemoveBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoveBackgroundApp.swift
+//  RemoveBackground
+//
+//  Created by Trần Ân on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct RemoveBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
