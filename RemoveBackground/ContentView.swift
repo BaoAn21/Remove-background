a1ff891bed117eeb34fc5269f781b6b2e128298f
AnTran <cao.an99@gmail.com>
2024-07-19 17:55:14 +0700

    commit

--- RemoveBackground/ContentView.swift
+++ RemoveBackground/ContentView.swift
@@ -8,17 +8,30 @@
 import SwiftUI
 import Vision
 import CoreImage.CIFilterBuiltins
+import PhotosUI
 
 struct ContentView: View {
-    @State private var image = UIImage.cat
-    @State private var isLoading: Bool = false
-    private var oriImage: UIImage = UIImage.cat
-    
+//    @State private var image = UIImage.cat
+//    @State private var isLoading: Bool = false
+    @State private var oriImage: UIImage?
+//
+//    var body: some View {
+            
+    @State private var avatarItem: PhotosPickerItem?
+    @State private var image: UIImage? // Change to UIImage
+
     var body: some View {
-            VStack {
-                Image(uiImage: image)
+        VStack {
+            PhotosPicker("Select avatar", selection: $avatarItem, matching: .images)
+
+            if let avatarImage = image { // Display if available
+                Image(uiImage: avatarImage)
                     .resizable()
                     .scaledToFit()
+                    .frame(width: 500, height: 500)
+            }
+            
+            VStack {
                 VStack {
                     HStack {
                         Button("Create mask") {
@@ -35,29 +48,48 @@ struct ContentView: View {
                 
             }
             .padding()
+        }
+        .onChange(of: avatarItem) { newItem in // Update for newItem
+            Task {
+                if let data = try? await newItem?.loadTransferable(type: Data.self),
+                   let image = UIImage(data: data) {
+                    self.image = image
+                    self.oriImage = image
+                } else {
+                    print("Failed")
+                }
+            }
+        }
     }
+    
 
     // MARK: - Private
     private func createMask() {
-        guard let processedImage = createMaskFromImage(fromImage: image) else {
-            print("error creat mask")
-            return
+        if (image != nil) {
+            guard let processedImage = createMaskFromImage(fromImage: image!) else {
+                print("error creat mask")
+                return
+            }
+            let finalimage = renderFromCItoUI(ciImage: processedImage)
+            self.image = finalimage
         }
-        let image = renderFromCItoUI(ciImage: processedImage)
-        self.image = image
+        
     }
     
     private func createSticker() {
-        guard let createCIimage = CIImage(image: image) else {
-            print("Can not create CI Image")
-            return
+        if (image != nil) {
+            guard let createCIimage = CIImage(image: image!) else {
+                print("Can not create CI Image")
+                return
+            }
+            guard let processedImage = createMaskFromImage(fromImage: image!) else {
+                print("error creat mask")
+                return
+            }
+            let image = applyFromMaskToImage(maskImage: processedImage, to: createCIimage)
+            self.image = renderFromCItoUI(ciImage: image)
         }
-        guard let processedImage = createMaskFromImage(fromImage: image) else {
-            print("error creat mask")
-            return
-        }
-        let image = applyFromMaskToImage(maskImage: processedImage, to: createCIimage)
-        self.image = renderFromCItoUI(ciImage: image)
+        
     }
 }
 
